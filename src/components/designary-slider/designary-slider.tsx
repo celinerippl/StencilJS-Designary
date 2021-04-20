1c1c0f100fcca292c24b856d820bc22f4ac5452a
BluescreenCreator <81440569+BluescreenCreator@users.noreply.github.com>
2021-04-20 17:14:21 +0200

    created DesingarySlider

---
+++ src/components/designary-slider/designary-slider.tsx
@@ -0,0 +1,12 @@
+import { Component } from '@stencil/core' ;
+
+@component({
+    tag: 'designary-slider'
+    styleUrl : 'designary-slider.css'
+})
+
+export class DesignarySlider {
+    render() {
+        
+    }
+}
